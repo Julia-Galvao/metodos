acb3cee98b03f018b61a1c80081121efbf8de1dc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-30 20:51:12 -0300

    novo

--- src/exemplo1.java
+++ src/exemplo1.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 public class exemplo1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int valor1, valor2;
+        int valor1, valor2,resultado;
 
         System.out.print("Valor 1: ");
         valor1 = sc.nextInt();
         System.out.print("Valor 2: ");
         valor2 = sc.nextInt();
 
-        somar(valor1, valor2); // argumentos --> valores que serão enviados/copiados para outro metodo
+       resultado =  somar(valor1, valor2); // argumentos --> valores que serão enviados/copiados para outro metodo
+        System.out.println("Resultado = " +resultado);
     }
-    public static void somar(int valor1, int valor2){ // parametro do metodo
+    public static int somar(int valor1, int valor2){ // parametro do metodo
         int resultado;
         resultado = valor1 + valor2;
-        System.out.println("Resultado = " +resultado);
+        return resultado;
+
     }
 }
