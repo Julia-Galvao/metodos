66c48fb101b89e008401f9d5d3bb2071f53a565f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-07 20:44:05 -0300

    ex2

--- src/exercicio2.java
+++ src/exercicio2.java
@@ -3,6 +3,36 @@ import java.util.Scanner;
 public class exercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        double lado1, lado2, lado3;
 
+        System.out.println("Lado 1  --> ");
+        lado1 = sc.nextByte();
+        System.out.println("Lado 2 --> ");
+        lado2 = sc.nextDouble();
+        System.out.println("Lado 3 --> ");
+        lado3 = sc.nextDouble();
+
+        if (validar(lado1,lado2,lado3)) {
+            classificar(lado1,lado2,lado3);
+        }
+        else {
+            System.out.println("Os valores nã formam um triângulo");
+        }
+    }
+
+    public static boolean validar (double l1, double l2, double l3){ //boolean tem retorno de valor
+        return (l1 < l2 + l3 && l2 < l1 + l3 && l3 < l1 + l2);
+    }
+
+    public static void classificar (double l1, double l2, double l3) { //void significa que não tem retorno de valor
+        if (l1 == l2 && l2 == l3){
+            System.out.println("Triâgulo equilátero");
+        }
+        else if (l1 == l2 || l1 == l3 || l2 == l3) {
+            System.out.println("Triâgulo isósceles ");
+        }
+        else {
+            System.out.println("Triâgulo escaleno");
+        }
     }
 }
